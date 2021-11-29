40104d2ebd197efa6d780413f115389813b21297
emelieasbrink <emelie.asbrink@gmail.com>
2021-11-29 14:37:28 +0100

    flyttat till app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@
 </template>
 
 <style>
+@import 'https://fonts.googleapis.com/css?family=Average';
+
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: Average, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-size: 14pt;
+}
+
+#app button{
+  font-family: Average, sans-serif;
 }
 
 #nav {
