0b84b885ea937325603a544ec8a310169faeb019
Svante <svante.lars.sundberg@gmail.com>
2021-12-10 18:09:45 +0100

    lite knappar till pollId

--- src/views/pollID.vue
+++ src/views/pollID.vue
@@ -1,26 +1,39 @@
 <template>
   <div class='standard'>
+  <div class= "write" v-if="done===false">
+    <h1>
+      {{uiLabels.writePollID}}
+    </h1>
+    <br>
+    <input type="text" v-model="id" maxlength="15" placeholder="Type ID">
 
-  <label v-if="done===false">
-    {{uiLabels.writePollID}}
-    <input type="text" v-model="id">
-    <button v-on:click="checkPollId">
-      Done
-    </button>
-  </label>
+  <button class="done" v-on:click="checkPollId" v-if="this.id.length>1">
+    Done
+  </button>
+</div>
 
-  <label v-if="done===true">
-    välj namn
-    <input type="text" v-model="userName">
 
-    <button id="createID" v-on:click="sendUsername" >
-    {{uiLabels.Done}} {{this.uniqueName}}
+
+  <div class="write" v-if="done===true && !ready">
+    <h1>
+      Välj namn
+    </h1>
+    <input type="text" v-model="userName" maxlength="15">
+    <button class="done" v-on:click="sendUsername" v-if="!ready && this.userName.length>1">
+    {{uiLabels.Done}}
 
   </button>
-<router-link v-if="!uniqueName" v-bind:to="'/poll/'+id+'/'+lang" tag="button">
-</router-link>
-<span v-else> upptaget </span>
-  </label>
+
+  </div>
+
+  <div id="readyGo" v-if="ready">
+    <h1> Get ready!</h1>
+    <h2>Your username is: <span style="text-decoration:underline">{{this.userName}}</span></h2>
+    <br>
+  <router-link  v-bind:to="'/poll/'+id+'/'+lang" tag="button">
+    <button class="done"> Lets start quizzing</button>
+  </router-link>
+</div>
   </div>
 </template>
 
@@ -37,7 +50,8 @@ export default {
       lang: "",
       done:false,
       userName:"",
-      uniqueName:false
+      uniqueName:false,
+      ready:false
     }
   },
   created: function () {
@@ -58,6 +72,9 @@ export default {
       if(existing){
         alert('Namn upptaget')
       }
+      else {
+        this.ready=true;
+      }
     } );
   },
   methods: {
@@ -71,3 +88,41 @@ export default {
   }
 }
 </script>
+
+<style>
+.write input{
+  border-radius: 20px;
+  border: white;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  outline: none;
+  box-shadow: 0px 2px 2px rgba(0,0,0,0.11);
+
+}
+
+.write{
+  margin:auto;
+}
+
+.done{
+  background-color:#2ECC40;
+  border: 1px solid #2ECC40;
+  border-radius: 20px;
+  cursor: pointer;
+  color: #fff;
+  text-align:  center;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  margin-left:  10px;
+}
+
+.done:hover{
+  border: 1px solid #fff;
+
+}
+
+</style>
