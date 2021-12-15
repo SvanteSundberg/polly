403e33242d249af967f717a0de701c1368dec5d5
emelieasbrink <emelie.asbrink@gmail.com>
2021-12-15 09:57:33 +0100

    createID

--- src/views/createID.vue
+++ src/views/createID.vue
@@ -5,28 +5,36 @@
   <br>
   <input type="text" v-model="pollId" :placeholder="uiLabels.typeID">
 </label>
-<router-link v-bind:to="'/selectTheme/'+pollId+'/'+lang">
-  <br>
-  <br>
-<button class="buttonDone" v-on:click="createPoll">
+
+<button class="done" v-on:click="checkID">
   {{uiLabels.Done}}
 </button>
-</router-link>
-{{this.pollId}}
+
+<createPopup v-on:stop="showPopup(false)"
+            v-show="this.popupVisable">
+<template v-slot:header> Quizzer </template>
+<span> ID already exists </span>
+</createPopup>
+
 </div>
 
 </template>
 
 <script>
+import createPopup from '@/components/createPopup.vue';
 import io from 'socket.io-client';
 const socket = io();
 export default {
   name: 'CreateID',
+  components: {
+    createPopup
+  },
   data: function () {
     return {
       lang: "",
       pollId: "",
-      uiLabels: {}
+      uiLabels: {},
+      popupVisable: false
     }
   },
   created: function () {
@@ -35,15 +43,36 @@ export default {
     socket.on("init", (labels) => {
       this.uiLabels = labels
     });
-    },
-    methods: {
-      createPoll: function () {
-        socket.emit("createPoll", {pollId: this.pollId, lang: this.lang });
 
-      },
+    socket.on('existingPolls', (existing)=>{
+      if (existing){
+        this.showPopup(true);
+      }
+      else{
+        socket.emit("createPoll", {pollId: this.pollId, lang: this.lang });
+        this.$router.replace('/selectTheme/'+this.pollId+'/'+this.lang);
+      }
+    });
     },
+
+    methods: {
+      checkID: function(){
+        console.log("checking");
+        if (this.pollId.length>0){
+          socket.emit('checkPollId', this.pollId);
+        }
+        else{
+          this.showPopup(true);
+        }
+      },
+      showPopup:function(value){
+        this.popupVisable=value;
+      },
+
+  }
   }
 </script>
+
 
 <style>
 .buttonDone{
